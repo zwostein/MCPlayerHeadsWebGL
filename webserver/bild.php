09947c35c4c19273bee8d4ba9e313708a5636a94
sh4ni <kittens@t-online.de>
2012-11-25 18:27:01 +0100

    changes for php texture loader

---
+++ webserver/bild.php
@@ -0,0 +1,27 @@
+<?php
+    header("Content-type: image/png");
+    $mcname	= $_GET['name'];
+
+    $piclink = "cache/" . $mcname . ".png";
+    if ( file_exists($piclink) ) {
+        $ftime = filemtime($filename); // hier wird noch gearbeitet
+    }
+    else {
+        $source = "http://s3.amazonaws.com/MinecraftSkins/" . $mcname . ".png";
+        $content = @file_get_contents($source);
+        if ( $content ) {
+            $destination = $piclink;
+            file_put_contents($destination , $content);
+        }
+    }
+    
+    $pic = @imagecreatefrompng($piclink);
+    if ( !$pic ) {
+        $pic = imagecreatefrompng("steve.png");
+    }
+
+    imagealphablending($pic, true);
+    imagesavealpha($pic, true);
+    imagepng($pic);
+    imagedestroy($pic);
+?>
