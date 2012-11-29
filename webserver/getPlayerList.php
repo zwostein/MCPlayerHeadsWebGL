62dfceff906f89798577c2438927f0e3c0d20fae
sh4ni <kittens@t-online.de>
2012-11-30 00:07:12 +0100

    defaultplayer if nobody is playing
    * getPlayerList can handle strings and has a defaultplayer is nobody is
    playing
    * small improvements

--- webserver/getPlayerList.php
+++ webserver/getPlayerList.php
@@ -1,4 +1,20 @@
 <?php
+
+$mcname = "Zw0 sh4ni";		// Hardcode for testing
+$defaultskin = "jeb_";		// Defaultskin if nobody is playing
+
 header('Content-type: text/plain');
-print( '[ "Zw0", "sh4ni", "jeb_", "kaylabs" ]' );
+$name = explode( " ", $mcname );
+$anzahl = count($name);
+print( '[ ' );
+if ( $mcname ) {
+	for ( $i = 0; $i < $anzahl; $i++ ) {
+		print( '"' . $name[$i] . '"' );
+		if ( $i < $anzahl -1 ) print( ',' );
+	}
+}
+else {
+	print( '"' . $defaultskin . '"' );
+}
+print( ' ]' );
 ?>
--- webserver/getTexture.php
+++ webserver/getTexture.php
@@ -4,21 +4,21 @@ $mcname	= $_GET['name'];
 
 $piclink = "cache/" . $mcname . ".png";
 
-//if ( file_exists($piclink) ) {
-//	$ftime = filemtime($filename); // hier wird noch gearbeitet
-//}
-//else {
-$source = "http://s3.amazonaws.com/MinecraftSkins/" . $mcname . ".png";
-$content = @file_get_contents($source);
+if ( file_exists($piclink) ) {
+	$ftime = filemtime($piclink); // hier wird noch gearbeitet
+}
+else {
+	$source = "http://s3.amazonaws.com/MinecraftSkins/" . $mcname . ".png";
+	$content = @file_get_contents($source);
 	if ( $content ) {
 		$destination = $piclink;
-		file_put_contents($destination, $content);
+		@file_put_contents($destination, $content);
 	}
-//}
+}
 
 $pic = @imagecreatefrompng($piclink);
 if ( !$pic ) {
-	$pic = imagecreatefrompng("steve.png");
+	$pic = @imagecreatefrompng("steve.png");
 }
 
 imagealphablending($pic, true);
